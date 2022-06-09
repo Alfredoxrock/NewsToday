35be7dad69bfe9eb763610b92f93fc2989cc3012
Alfredoxrock <alf_baza@hotmail.com>
2022-06-08 19:07:54 -0600

    Fixed paths bug

--- NewsToday/MainWindow.xaml.cs
+++ NewsToday/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Collections.ObjectModel;
-using NewsToday.Database;
+//using NewsToday.Database;
 
 namespace NewsToday
 {
@@ -30,7 +30,7 @@ namespace NewsToday
             //Data.articles.Reverse();
             Helper.loadArticlePreview();
             InitializeComponent();
-            DatabaseHelper.EstablishConnection();
+            //DatabaseHelper.EstablishConnection();
 
             hideBar();
 
